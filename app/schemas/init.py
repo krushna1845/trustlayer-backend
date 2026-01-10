1fdd48cb237347439457191c13869d44aca5a4c4
Krushna Malode <krushna.2403504105@vcet.edu.in>
2026-01-10 12:24:00 +0530

    Import schemas and define __all__ for exports

--- app/schemas/init.py
+++ app/schemas/init.py
@@ -1,1 +1,9 @@
+from app.schemas.user import UserCreate, UserLogin, UserResponse, Token
+from app.schemas.contract import ContractCreate, ContractResponse, ContractList
+from app.schemas.analysis import AnalysisResponse, ChatMessageCreate, ChatMessageResponse
 
+__all__ = [
+    "UserCreate", "UserLogin", "UserResponse", "Token",
+    "ContractCreate", "ContractResponse", "ContractList",
+    "AnalysisResponse", "ChatMessageCreate", "ChatMessageResponse"
+]
