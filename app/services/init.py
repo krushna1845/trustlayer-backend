063194b52d58f268ce2dd9f24f46233a0f3bb307
Krushna Malode <krushna.2403504105@vcet.edu.in>
2026-01-10 12:21:23 +0530

    Import services and define __all__ in init.py

--- app/services/init.py
+++ app/services/init.py
@@ -1,1 +1,5 @@
+from app.services.auth_service import AuthService
+from app.services.contract_service import ContractService
+from app.services.ai_service import AIService
 
+__all__ = ["AuthService", "ContractService", "AIService"]
