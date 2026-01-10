c780421f12c582154c6a19a33d78a523bf1f5f73
Krushna Malode <krushna.2403504105@vcet.edu.in>
2026-01-10 12:25:20 +0530

    Update init.py

--- app/models/init.py
+++ app/models/init.py
@@ -1,1 +1,5 @@
+from app.models.user import User
+from app.models.contract import Contract
+from app.models.analysis import Analysis, ChatMessage
 
+__all__ = ["User", "Contract", "Analysis", "ChatMessage"]
